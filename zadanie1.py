7ef0fed8fe07913ec9a9946f44f53a7ec92a6043
zazadrevesnaya <petroosetro0@gmail.com>
2025-06-03 20:53:18 +0300

    Update zadanie1.py

--- zadanie1.py
+++ zadanie1.py
@@ -3,39 +3,50 @@ import time
 import random
 
 def writing_file(filename, data):
-        with open(f"{filename}.txt", 'w', encoding='utf-8') as file:
-            file.write(data)
-            time.sleep(1)
-        print(f"Данные записаны в файл {filename}")
+    with open(f"{filename}.txt", 'w', encoding='utf-8') as file:
+        file.write(data)
+        time.sleep(1)
+    print(f"Данные записаны в файл {filename}")
 
-#парралельное выполнение
-start1 = time.time()
-threads = []
-for i in range(10):
-    num = [random.randint(1,100) for o in range(100)]
-    thread = threading.Thread(target = writing_file, args=(f"file_{i}" , f"num = {num}"))
-    thread.start()
-    threads.append(thread)
-    res1 = time.time()-start1
+def parallel():
+    global res1  # Делаем переменную глобальной
+    start1 = time.time()
+    threads = []
 
-time.sleep(5)
-#последовательное выполнение
-print('последовательное')
-start2 = time.time()
-for i in range(10):
-    num = [random.randint(1,100) for o in range(100)]
-    writing_file(f"file_1{i}" , f"num = {num}") 
-res2 = time.time() - start2
-print('последовательно',time.time() - start2)
+    for i in range(10):
+        num = [random.randint(1,100) for _ in range(100)]
+        thread = threading.Thread(target=writing_file, args=(f"file_{i}", f"num = {num}"))
+        thread.start()
+        threads.append(thread)
 
-for t in threads:
-     t.join()
-print("result1=", res1)
-print("result2=", res2)
-print('результаты:')
-if res1 > res2:
-    print('res1 > res2')
-elif (res1 < res2):
-    print('res1 < res2')
-else:
-    print('res1 = res2')
+    for t in threads:
+        t.join()
+
+    res1 = time.time() - start1
+    print("Параллельное выполнение:", res1)
+    time.sleep(5)
+
+def posledovat():
+    global res1  
+    print('Последовательное выполнение')
+    start2 = time.time()
+
+    for i in range(10):
+        num = [random.randint(1,100) for _ in range(100)]
+        writing_file(f"file_1{i}", f"num = {num}")
+
+    res2 = time.time() - start2
+    print('Последовательно:', res2)
+
+    print("Результаты:")
+    if res1 > res2:
+        print('Параллельное выполнение дольше')
+    elif res1 < res2:
+        print('Последовательное выполнение дольше')
+    else:
+        print('Одинаковое время выполнения')
+
+def main1():
+    parallel()
+    posledovat()
+
