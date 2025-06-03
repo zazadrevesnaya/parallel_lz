47c91e85ae8fd3007c8a7f0b6b631862e85e3062
zazadrevesnaya <petroosetro0@gmail.com>
2025-06-03 10:41:50 +0300

    Update zadanie2.py

--- zadanie2.py
+++ zadanie2.py
@@ -1,23 +1,5 @@
 import multiprocessing
 
-def appending_numbers(number):
-    with open("data.txt", "a") as file:
-        file.write(f'{number}\n')
-
-if __name__ == "__main__":
-    filename = "data.txt"
-    with open(filename, "w") as file:
-        file.write("")
-    numbers = range(100)  
-    with multiprocessing.Pool(processes=10) as pool:
-        pool.map(appending_numbers, numbers)
-    print('файлы записаны')
-
-
-
-
-import multiprocessing
-
 lock = 0
 def init_lock(l):
     global lock
